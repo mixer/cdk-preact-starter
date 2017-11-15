010deff2f507afd99f101dad1a32fd6bd7803677
Connor Peet <connor@peet.io>
2017-11-15 15:58:57 -0800

    feat(alchemy+prefab): add HTML control and control prefab

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as Mixer from '@mcph/miix-std';
 import { h, render } from 'preact';
+import { HtmlStringControl } from './prefabs/html/html';
 
 import { PreactStage } from './alchemy/preact/index';
 import { Button as ButtonControl } from './prefabs/button/button';
@@ -11,7 +12,12 @@ require('./style.scss');
 
 // The registry contains a list of all your custom scenes and buttons. You
 // should pass them in here so that we're aware of them!
-const registry = new Mixer.Registry().register(ButtonControl, JoystickControl, DefaultScene);
+const registry = new Mixer.Registry().register(
+  HtmlStringControl,
+  ButtonControl,
+  JoystickControl,
+  DefaultScene,
+);
 
 // Do the thing!
 render(<PreactStage registry={registry} />, document.querySelector('#app'));
