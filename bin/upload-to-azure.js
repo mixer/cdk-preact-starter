46836253303f755173fb3683ce44c1942644d202
Derek Jensen <derek.jensen@microsoft.com>
2017-12-06 17:35:56 -0800

    tar and upload the whole source, not build

--- bin/upload-to-azure.js
+++ bin/upload-to-azure.js
@@ -2,7 +2,9 @@
 
 const webpack = require('webpack');
 const { execSync } = require('child_process');
-const targz = require('targz');
+const tar = require('tar');
+const glob = require('glob');
+const fs = require('fs');
 const wpconfig = require('../webpack.config');
 const { version } = require('../package.json');
 
@@ -20,28 +22,47 @@ webpack(wpconfig, err => {
 
   console.log(' → Compressing files to tarball');
 
-  targz.compress({
-      src: 'build',
-      dest: `build/${blobName}`
-  }, err => {
-      if (err) {
-          console.error(err);
-          return process.exit(1);
-      }
+  const ignore = fs.readFileSync('.gitignore', {encoding: 'utf8'})
+  .split('\n')
+  .filter(pattern => !!pattern && pattern[0] !== '#' && pattern[0] !== '!') // Removing Comments & Files to be negated in gitignore.
+  .map(pattern => pattern + (pattern.split('/').pop().indexOf('.') < 0 ? '/**' : '')) // Add proper filtering for folders.
+  ignore.push('bin/**'); // Let's exclude the goodies in bin as well.
 
-      console.log(' ✔ Files compressed')
+  glob('**/**', {ignore: ignore}, (err, files) => {
+    if (err) {
+      console.error(err);
+      return process.exit(1);
+    }
 
-      console.log(' → Upload to Azure');
-      execSync([
-        'az storage blob upload',
-        '--content-type "application/gzip"',
-        '-c controls',
-        `-f ../build/${blobName}`,
-        `-n ${blobName}`
-      ].join(' '), {
-        cwd: __dirname,
-        env: process.env,
-      });
-      console.log(' ✔ Uploaded to Azure');
-  })
+    tar.c(
+        {
+          gzip: true,
+          file: `build/${blobName}`
+        },
+        files,
+        (err, res) => {
+            if (err) {
+              console.error(err);
+              return process.exit(1);
+            }
+
+            console.log(' ✔ Files compressed');
+
+            console.log(' → Upload to Azure');
+
+            execSync([
+            'az storage blob upload',
+            '--content-type "application/gzip"',
+            '-c controls',
+            `-f ../build/${blobName}`,
+            `-n ${blobName}`
+            ].join(' '), {
+            cwd: __dirname,
+            env: process.env,
+            });
+
+            console.log(' ✔ Uploaded to Azure');
+        }
+      )
+  });
 });
