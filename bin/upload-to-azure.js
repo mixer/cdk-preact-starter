00c3cb6e32b1021522845fcb9510c7c3088fc54b
Derek Jensen <derek.jensen@microsoft.com>
2017-12-07 11:12:18 -0800

    Remove useless webpack compilation, add path handle running the script from any directory

--- bin/upload-to-azure.js
+++ bin/upload-to-azure.js
@@ -1,58 +1,47 @@
 #!/usr/bin/env node
 
-const webpack = require('webpack');
 const { execSync } = require('child_process');
 const tar = require('tar');
 const globby = require('globby');
 const fs = require('fs');
-const wpconfig = require('../webpack.config');
+const path = require('path');
 const { version } = require('../package.json');
 
 const [major, minor] = version.split('.');
 const blobName = `interactive-launchpad_${major}.${minor}.tar.gz`;
 
-console.log(' → Compiling library');
-webpack(wpconfig, err => {
-  if (err) {
-    console.error(err);
-    return process.exit(1);
-  }
+console.log(' → Compressing files to tarball');
 
-  console.log(' ✔ Compiled library');
+(async () => {
+  const paths = await globby(['**/**', '!bin/*'], { gitignore: true });
+  tar.c(
+    {
+      gzip: true,
+      file: `build/${blobName}`
+    },
+    paths,
+    (err, res) => {
+      if (err) {
+        console.error(err);
+        return process.exit(1);
+      }
 
-  console.log(' → Compressing files to tarball');
+      console.log(' ✔ Files compressed');
 
-  (async () => {
-    const paths = await globby(['**/**', '!bin/*'], { gitignore: true });
-    tar.c(
-      {
-        gzip: true,
-        file: `build/${blobName}`
-      },
-      paths,
-      (err, res) => {
-        if (err) {
-          console.error(err);
-          return process.exit(1);
-        }
+      console.log(' → Upload to Azure');
 
-        console.log(' ✔ Files compressed');
+      execSync([
+        'az storage blob upload',
+        '--content-type "application/gzip"',
+        '-c controls',
+        `-f build/${blobName}`,
+        `-n ${blobName}`
+        ].join(' '), {
+          cwd: path.resolve(__dirname, '..'),
+          env: process.env,
+      });
 
-        console.log(' → Upload to Azure');
-
-        execSync([
-          'az storage blob upload',
-          '--content-type "application/gzip"',
-          '-c controls',
-          `-f ../build/${blobName}`,
-          `-n ${blobName}`
-          ].join(' '), {
-            cwd: __dirname,
-            env: process.env,
-        });
-
-          console.log(' ✔ Uploaded to Azure');
-        }
-      )
-  })();
-});
+        console.log(' ✔ Uploaded to Azure');
+      }
+    )
+})();
