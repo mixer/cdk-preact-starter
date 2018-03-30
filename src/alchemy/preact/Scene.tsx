2f41880cbc79f2766c609bf9ce89adc0d388a0b2
Derek Jensen <derek.jensen@microsoft.com>
2018-03-30 16:51:24 -0700

    feat(screen): touch events

--- src/alchemy/preact/Scene.tsx
+++ src/alchemy/preact/Scene.tsx
@@ -5,20 +5,24 @@ import { MScene } from '../State';
 import { untilUnmount } from '../Toolbox';
 import { FixedGridLayout, FlexLayout } from './Layout';
 
-export type SceneProps<S> = { resource: MScene<S & Mixer.IScene> } & S & Mixer.IScene;
+export type SceneProps<S> = { resource: MScene<S & Mixer.IScene> } & S &
+  Mixer.IScene;
 
 /**
  * PreactScene is the base scene. You can extend and override this scene.
  */
 export abstract class PreactScene<T, S = {}> extends Component<
   SceneProps<S>,
-  T & { settings: Mixer.ISettings }
+  T & { settings: Mixer.ISettings; containers: Mixer.Layout.IContainer[] }
 > {
   protected scene: MScene<S & Mixer.IScene>;
 
   constructor(props: SceneProps<S>) {
     super(props);
     this.scene = props.resource;
+    this.setState(
+      Object.assign({}, this.state, { containers: props.containers }),
+    );
   }
 
   /**
@@ -31,6 +35,7 @@ export abstract class PreactScene<T, S = {}> extends Component<
       .subscribe((settings: Mixer.ISettings) => {
         this.setState(Object.assign({}, this.state, { settings }));
       });
+    this.updateStateContainers(this.props);
   }
 
   /**
@@ -38,16 +43,42 @@ export abstract class PreactScene<T, S = {}> extends Component<
    */
   public componentWillReceiveProps(nextProps: SceneProps<S>) {
     this.scene = nextProps.resource;
+    this.updateStateContainers(nextProps);
   }
 
   /**
-   * Returns the layout engine that these controls are using.
+   * Returns the layout engines that these controls are using.
    */
-  protected getLayoutEngine() {
-    if (this.scene.get('containers')) {
+  protected getFixedGridLayoutEngine() {
+    if (this.props.controls.find(control => control.kind !== 'screen')) {
+      return FixedGridLayout;
+    }
+    return null;
+  }
+
+  protected getFlexLayoutEngine() {
+    if (
+      this.state.containers ||
+      this.props.controls.find(control => control.kind === 'screen')
+    ) {
       return FlexLayout;
     }
+    return null;
+  }
 
-    return FixedGridLayout;
+  private updateStateContainers(props: SceneProps<S>) {
+    const screenControl = props.controls.find(
+      control => control.kind === 'screen',
+    );
+    if (screenControl) {
+      const newContainers = Object.assign([], this.state.containers, [
+        {
+          children: [{ controlID: screenControl.controlID }],
+        },
+      ]);
+      this.setState(
+        Object.assign({}, this.state, { containers: newContainers }),
+      );
+    }
   }
 }
