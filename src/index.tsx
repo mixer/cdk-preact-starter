847a9daae199694e0df5607ca740e92f818a5cfd
Derek Jensen <derek.jensen@microsoft.com>
2018-04-24 14:12:44 -0700

    chore(*): remove screen until complete

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import { Button as ButtonControl } from './prefabs/button/button';
 import { DefaultScene } from './prefabs/default-scene/scene';
 import { Joystick as JoystickControl } from './prefabs/joystick/joystick';
 import { Label as LabelControl } from './prefabs/label/label';
-import { Screen as ScreenControl } from './prefabs/screen/screen';
 import { TextBox as TextboxControl } from './prefabs/textbox/textbox';
 // Import our custom CSS.
 require('./style.scss');
@@ -20,7 +19,6 @@ const registry = new Mixer.Registry().register(
   JoystickControl,
   DefaultScene,
   LabelControl,
-  ScreenControl,
   TextboxControl,
 );
 
